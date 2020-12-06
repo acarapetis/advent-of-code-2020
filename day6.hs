a13a6b8ddc6c4f6e0ac88e9c2ad6a275a8edcc7a
Anthony Carapetis <anthony.carapetis@gmail.com>
2020-12-06 16:36:19 +1100

    slight refactor

--- day6.hs
+++ day6.hs
@@ -1,24 +1,20 @@
+import Text.Parsec
 import qualified Data.Set as S
 import Control.Applicative ((<*))
-import Data.Set (member)
-import Data.Either (fromRight)
-import Text.Parsec
 
-part1 = show . sum . map groupScore . parseGroups'
+part1 = show . sum . map groupScore . parseGroups
 groupScore = S.size . S.unions
 
-person = many1 letter
+person = fmap S.fromList $ many1 letter
 group = many1 (person <* endOfLine)
 groups = sepBy1 group endOfLine
 
-parseGroups = parse groups ""
-parseGroups' = map (map S.fromList) . _pg . parseGroups where
+parseGroups = _pg . parse groups "" where
     _pg (Left x) = error $ show x
     _pg (Right x) = x
-
     
 groupScore2 = S.size . foldl1 S.intersection
-part2 = show . sum . map groupScore2 . parseGroups'
+part2 = show . sum . map groupScore2 . parseGroups
 
 main = do
     input <- readFile "day6.txt"
