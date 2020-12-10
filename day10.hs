d73a3300d5734a609384415398d7b769e7ecf9b5
Anthony Carapetis <anthony.carapetis@ga.gov.au>
2020-12-10 18:28:13 +1100

    minor cleanup of day10

--- day10.hs
+++ day10.hs
@@ -1,14 +1,11 @@
-import AOC
-import qualified Data.Set as S
-import Data.List (find, sort, zipWith, group)
+import Data.List (sort, zipWith, group)
 
-readNums :: String -> [Int]
 readNums txt = 0:(map read . lines $ txt)
 
 deltas nums = zipWith (-) (tail nums) nums
 numOf x = length . filter (==x)
 
-part1 nums = show $ ((f 3) + 1) * (f 1)
+part1 nums = show $ (f 3 + 1) * f 1
     where f n = numOf n (deltas nums)
 
 -- by inspection, input data has deltas of either 3 or 1;
@@ -17,18 +14,16 @@ part1 nums = show $ ((f 3) + 1) * (f 1)
 
 -- number of paths through a chain of n length-1 differences jumps,
 -- memoized:
-arrC' n
-    | n == 0 = 1
-    | n == 1 = 1
-    | n == 2 = 2
-    | otherwise = arrC (n-1) + arrC (n-2) + arrC (n-3)
-arrCache = map arrC' [0..]
-arrC = (arrCache!!)
+arrC = (map arrC' [0..] !!) where
+    arrC' 0 = 1
+    arrC' 1 = 1
+    arrC' 2 = 2
+    arrC' n = arrC (n-1) + arrC (n-2) + arrC (n-3)
 
 get1chains = map length . filter (\x -> head x == 1) . group
 part2 = show . product . map arrC . get1chains . deltas
 
 main = do
-    nums <- fmap (sort . readNums) $ readFile "day10.txt"
+    nums <- sort . readNums <$> readFile "day10.txt"
     putStrLn $ part1 nums
     putStrLn $ part2 nums
