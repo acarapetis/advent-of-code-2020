c35a3e07c0e678b4442e64824a4b481af6f80a96
Anthony Carapetis <anthony.carapetis@ga.gov.au>
2020-12-17 15:43:23 +1100

    day11 part 1

--- Grid.hs
+++ Grid.hs
@@ -1,4 +1,4 @@
-module Grid (module Grid, bounds) where
+module Grid (module Grid, bounds, elems) where
 
 import Data.Array
 import Data.List (intercalate)
@@ -19,9 +19,9 @@ subtractC u v = addC u (negateC v)
 -- This is unsafe: it assumes the elements of xs all have equal length.
 fromListOfLists :: [[a]] -> Grid a
 fromListOfLists xs = array (min, max) associations where
-    min@(i0,j0) = (0,0)
-    max@(i1,j1) = (length xs - 1, length (head xs) - 1)
-    associations = [((i,j), (xs!!i)!!j) | i <- [i0..i1], j <- [j0..j1]]
+    min = (0,0)
+    max = (length xs - 1, length (head xs) - 1)
+    associations = [((i,j), (xs!!i)!!j) | (i,j) <- range (min, max)]
 
 width grid = xmax - xmin + 1 where ((_, xmin), (_, xmax)) = bounds grid
 height grid = ymax - ymin + 1 where ((ymin, _), (ymax, _)) = bounds grid
@@ -37,11 +37,11 @@ offset delta grid = ixmap (addC start delta, addC end delta) (`subtractC` delta)
     where (start, end) = bounds grid
 
 convolveWith ::
-    (a -> b -> c) -- function to combine values sampled from the two grids
-    -> (e -> c -> e) -- function to accumulate the combined values
-    -> e -- starting value for each accumulator
+    (a -> b -> c)        -- function to combine values sampled from the two grids
+    -> (c -> c -> c)     -- function to accumulate the combined values
+    -> c                 -- starting value for each accumulator
     -> Grid a -> Grid b  -- input grids
-    -> Grid e
+    -> Grid c
 convolveWith combine accum start f g = accumArray accum start (min, max) associations where
     min = addC minf ming
     max = addC maxf maxg
@@ -54,3 +54,23 @@ convolveWith combine accum start f g = accumArray accum start (min, max) associa
 -- traditional linear convolution
 convolve :: Num a => Grid a -> Grid a -> Grid a
 convolve = convolveWith (*) (+) 0
+
+trim :: (Coord, Coord) -> Grid a -> Grid a
+trim (min, max) = array (min, max) . filter inBounds . assocs
+    where inBounds (c, _) = inRange (min, max) c
+
+trimmedConvolveWith ::
+    (a -> b -> c) -> (c -> c -> c) -> c -- as in convolveWith
+    -> Grid a -- KERNEL grid
+    -> Grid b -- DATA grid, output will be the same size
+    -> Grid c
+trimmedConvolveWith combine accum start kernel dat
+    = trim (bounds dat) $ convolveWith combine accum start kernel dat
+
+trimmedConvolve :: Num a => Grid a -> Grid a -> Grid a
+trimmedConvolve = trimmedConvolveWith (*) (+) 0
+
+zipGridsWith :: (a -> b -> c) -> Grid a -> Grid b -> Grid c
+zipGridsWith f x y
+    | bounds x /= bounds y = error "Incompatible grids"
+    | otherwise = listArray (bounds x) $ zipWith f (elems x) (elems y)
--- day11.hs
+++ day11.hs
@@ -1,27 +1,50 @@
+module Main where
+
 import AOC
-import Asm
-import qualified Data.Set as S
-import qualified Data.Map as M
-import Data.List (sort, zipWith, group)
+import Grid
+import Data.List (find)
+import Data.Maybe (fromJust)
 
-part1 txt = show ""
-part2 txt = show ""
 
-data Tile = Floor | EmptySeat | OccupiedSeat
-readTile '.' = Floor
-readTile 'L' = EmptySeat
-readTile '#'= OccupiedSeat
+data Tile = Floor | EmptySeat | OccupiedSeat deriving (Eq)
+instance Show Tile where
+    show Floor = "."
+    show EmptySeat = "L"
+    show OccupiedSeat ="#"
 
 tileP :: Parser Tile
-tileP = readTile <$> anyChar
+tileP = choice
+    [ Floor <$ char '.'
+    , EmptySeat <$ char 'L'
+    , OccupiedSeat <$ char '#'
+    ]
 rowP :: Parser [Tile]
 rowP = many1 tileP <* optional endOfLine
 gridP :: Parser [[Tile]]
 gridP = many1 rowP
 
+parseGrid = fromListOfLists . parseOrDie gridP
 
+neighbours = offset (-1, -1) $ fromListOfLists [[1,1,1],[1,0,1],[1,1,1]]
+occupiedNeighbours = trimmedConvolve neighbours . fmap occFlag
+    where occFlag OccupiedSeat = 1
+          occFlag _ = 0
+
+nextState :: Tile -> Int -> Tile
+nextState EmptySeat 0 = OccupiedSeat
+nextState OccupiedSeat n = if n>=4 then EmptySeat else OccupiedSeat
+nextState x _ = x
+
+step grid = zipGridsWith nextState grid (occupiedNeighbours grid)
+
+final :: Grid Tile -> Grid Tile
+final grid = fst . fromJust . find same $ zip gs (tail gs) where
+    same (g1, g2) = g1 == g2
+    gs = iterate step grid
+
+part1 = length . filter (==OccupiedSeat) . elems . final
+part2 txt = show ""
 
 main = do
-    nums <- sort . readNums <$> readFile "day10.txt"
-    putStrLn $ part1 nums
-    putStrLn $ part2 nums
+    grid <- parseGrid <$> readFile "day11.txt"
+    print $ part1 grid
